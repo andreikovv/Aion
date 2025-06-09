daed52eb9f04f4d6e3ec62783540ac2bbe75edfe
VladislaV <163776540+andreikovv@users.noreply.github.com>
2025-06-09 22:27:09 +0200

    Delete AION_CARE_Shop directory

--- AION_CARE_Shop/assets/js/lang.js
+++
@@ -1,9 +0,0 @@
-
-function setLanguage(lang) {
-    const messages = {
-        pl: "Zadbaj o siebie z AION CARE",
-        en: "Take care of yourself with AION CARE",
-        ua: "Подбай про себе з AION CARE"
-    };
-    document.getElementById("welcome-message").innerText = messages[lang];
-}
